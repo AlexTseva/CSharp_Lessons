b1c6dd61f5749b88b459602b6089a97b5e6bc9ab
«AlexTseva» <{kazantseva_as@mail.ru}>
2023-07-29 20:23:34 +0500

    part5

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 42;
-double numberB = 5;
+﻿double numberA = new Random().Next(10,35);
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1,5);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
