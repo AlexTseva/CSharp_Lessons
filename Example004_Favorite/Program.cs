72d27f0e715dddc1f2feace51603c38892128271
«AlexTseva» <{kazantseva_as@mail.ru}>
2023-07-30 17:40:49 +0500

    Add favorite

---
+++ Example004_Favorite/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Слава Маше!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
