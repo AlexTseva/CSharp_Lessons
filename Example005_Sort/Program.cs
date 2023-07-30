d54a12e657456694bfb8dc5048b976afd3bd666c
«AlexTseva» <{kazantseva_as@mail.ru}>
2023-07-31 00:29:20 +0500

    Add sort

---
+++ Example005_Sort/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дано: 5 гирь с разным весом в случайном порядке.
+// Найти: вес самой тяжелой гири.
+
+int a = 8;
+int b = 9;
+int c = 1;
+int d = 2;
+int e = 13;
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
